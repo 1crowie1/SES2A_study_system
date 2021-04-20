a3e97c5bfbca3a0cb8229caabdab035000d37c66
willhtang <will.h.tang@gmail.com>
2021-04-20 15:42:05 +1000

    Login system functional
    Should work...
    Link database ==> 'GroupFormationSystem'

--- study-system-app/src/Routes/StudentLogin/components/StudentLoginForm.js
+++ study-system-app/src/Routes/StudentLogin/components/StudentLoginForm.js
@@ -33,6 +33,7 @@ const StudentLoginForm = (props) => {
     console.log("LOGIN WITH GOOGLE");
     firebase.auth().signInWithPopup(provider).then((res) => {
       console.log(res.user)
+      props.history.push("/StudentHome");
     }).catch((error) => {
       console.log(error.message)
     });
@@ -42,10 +43,10 @@ const StudentLoginForm = (props) => {
     console.log("LOGIN WITH EMAIL");
     firebase.auth().signInWithEmailAndPassword(email, password).then((res) => {
       console.log(res.user)
+      props.history.push("/StudentHome");
     }).catch((error) => {
       console.log(error.message)
     });
-    props.history.push("/StudentHome");
   }
 
   return (
